39b5d82a4b50e56bd1498e62e3fbe9009cbdf006
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-08-23 02:18:42 +0530

    update: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import express from "express";
+import { startServer } from "./server";
 
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.get("/", (req, res) => {
-  res.send("Hi" + req.ip);
-});
-
-app.listen(port, () => console.log("Server http://localhost:" + port));
+startServer();
