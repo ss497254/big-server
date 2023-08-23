edc6b3e44d37d203857d35db16268c7bf10b0836
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-08-24 01:22:04 +0530

    update: add expresslogger in logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,7 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+
 const logger = {
   warn: console.warn,
   info: console.info,
   error: console.error,
+  trace: console.trace,
+  debug: console.debug,
+};
+
+export const expressLogger = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  console.log(`Req: ${req.hostname} ${req.path} ${req.ip}`);
+  next();
 };
 
 export default logger;
