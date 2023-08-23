2cbeea4eeb1feac82af74db803f14a33748b5851
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-08-24 01:17:56 +0530

    update: env config

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,119 +10,155 @@ const acceptedEnvTypes = [
   "json",
 ] as const;
 
-export interface IEnvVariable {
-  value: number | string | boolean | any[] | RegExp | undefined;
+export interface IEnvVariable<T = EnvVariablesType> {
+  value: T | undefined;
   type: (typeof acceptedEnvTypes)[number];
   setBy: "default" | "env" | "config-file" | "custom";
   deleteFromProcess?: boolean;
 }
 
-export type EnvKeysType =
-  | "NODE_ENV"
-  | "HOST"
-  | "PORT"
-  | "PUBLIC_URL"
-  | "CONFIG_PATH"
-  | "CORS_ENABLED"
-  | "CORS_ORIGIN"
-  | "CORS_METHODS"
-  | "CORS_ALLOWED_HEADERS"
-  | "CORS_EXPOSED_HEADERS"
-  | "CORS_CREDENTIALS"
-  | "CORS_MAX_AGE"
-  | "WEBSOCKETS_ENABLED"
-  | "SERVER_SHUTDOWN_TIMEOUT"
-  | "IP_TRUST_PROXY"
-  | "MAX_REQ_PAYLOAD_SIZE";
-
-const env: Record<EnvKeysType, IEnvVariable> = {
+const env = {
   NODE_ENV: {
     value: "development",
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   HOST: {
-    value: "127.0.0.1",
+    value: "localhost",
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   PORT: {
     value: "8080",
     type: "number",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   PUBLIC_URL: {
     value: undefined,
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
+  JWT_SECRET: {
+    value: "pA@#$RSdDF#%asgfda87",
+    type: "string",
+    setBy: "default",
+  } as IEnvVariable<string>,
+
   CONFIG_PATH: {
     value: undefined,
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   CORS_ENABLED: {
     value: false,
     type: "boolean",
     setBy: "default",
-  },
+  } as IEnvVariable<boolean>,
+
   CORS_ORIGIN: {
     value: undefined,
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   CORS_METHODS: {
-    value: undefined,
+    value: "GET,POST,PATCH,DELETE",
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   CORS_ALLOWED_HEADERS: {
     value: undefined,
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   CORS_EXPOSED_HEADERS: {
     value: undefined,
     type: "string",
     setBy: "default",
-  },
+  } as IEnvVariable<string>,
+
   CORS_CREDENTIALS: {
-    value: undefined,
-    type: "string",
+    value: true,
+    type: "boolean",
     setBy: "default",
-  },
+  } as IEnvVariable<boolean>,
+
   CORS_MAX_AGE: {
-    value: undefined,
-    type: "string",
-    setBy: "default",
-  },
-  SERVER_SHUTDOWN_TIMEOUT: {
-    value: 10,
+    value: 1000 * 60 * 24,
     type: "number",
     setBy: "default",
-  },
+  } as IEnvVariable<number>,
+
+  SERVER_SHUTDOWN_TIMEOUT: {
+    value: 10 * 1000,
+    type: "number",
+    setBy: "default",
+  } as IEnvVariable<number>,
+
+  RATE_LIMITER_GLOBAL_ENABLED: {
+    value: false,
+    type: "boolean",
+    setBy: "default",
+  } as IEnvVariable<boolean>,
+
   WEBSOCKETS_ENABLED: {
     value: true,
     type: "boolean",
     setBy: "default",
-  },
+  } as IEnvVariable<boolean>,
+
   IP_TRUST_PROXY: {
     value: 0,
     type: "number",
     setBy: "default",
-  },
+  } as IEnvVariable<number>,
+
   MAX_REQ_PAYLOAD_SIZE: {
     value: 1000 * 100,
     type: "number",
     setBy: "default",
-  },
-};
+  } as IEnvVariable<number>,
+
+  WEBSOCKETS_HEARTBEAT_PERIOD: {
+    value: 5 * 60 * 1000,
+    type: "number",
+    setBy: "default",
+  } as IEnvVariable<number>,
+
+  WEBSOCKETS_ENDPOINT: {
+    value: "/ws",
+    type: "string",
+    setBy: "default",
+  } as IEnvVariable<string>,
+
+  WEBSOCKETS_CONN_LIMIT: {
+    value: 20,
+    type: "number",
+    setBy: "default",
+  } as IEnvVariable<number>,
+} as const;
+
+export type EnvKeysType = keyof typeof env;
+export type EnvVariablesType =
+  | number
+  | string
+  | boolean
+  | RegExp
+  | Record<string, any>
+  | Record<string, any>[];
 
 export function getEnv() {
   return env;
 }
 
-export function getEnvConfig(key: EnvKeysType) {
+export function getEnvConfig(key: EnvKeysType): any {
   return env[key].value;
 }
 
@@ -170,13 +206,17 @@ function loadConfiguration() {
   );
 }
 
-function parseEnv(key: EnvKeysType, value: any, setBy: IEnvVariable["setBy"]) {
+function parseEnv(
+  key: EnvKeysType,
+  value: any,
+  setBy: IEnvVariable<Partial<EnvVariablesType>>["setBy"]
+) {
   const { type } = env[key];
 
   if (type === "string") env[key].value = value;
   else if (type === "boolean") env[key].value = toBoolean(value);
   else if (type === "number") env[key].value = parseFloat(value);
-  else if (type === "regex") env[key].value = new RegExp(value);
+  else if (type === "regex") env[key].value = new RegExp(value) as any;
   else if (type === "json") env[key].value = JSON.parse(value);
   else throw new Error(`Unexpected Env variable type for ${key}`);
 
