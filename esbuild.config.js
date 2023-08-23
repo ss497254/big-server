63cea2bd107e67d20d2aeb81e111e973bf229f61
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-08-24 01:17:00 +0530

    update: esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,22 @@
+const startTime = new Date().getTime();
+
 require("esbuild")
   .build({
-    entryPoints: ["src/index.ts"],
+    entryPoints: ["src/**/*.ts"],
     minify: true,
     format: "cjs",
-    bundle: true,
+    // bundle: true,
     platform: "node",
     outdir: "dist",
     color: true,
   })
   .then(console.log)
-  .catch(console.error);
+  .catch(console.error)
+  .finally(() =>
+    console.log(
+      // Print taken to complete the build
+      `\x1b[32m\x1b[1m
+        Done in ${(new Date().getTime() - startTime) / 1000}ms
+        \x1b[0m`
+    )
+  );
