af56b2bcfb36818197dacc81573d894071d1a016
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-08-28 02:00:30 +0530

    fix: user login validation

--- src/validations/user-auth.validation.ts
+++ src/validations/user-auth.validation.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const userLogin = z.object({
   body: z
     .object({
-      email: z.string({ required_error: "email is required" }),
+      username: z.string({ required_error: "username is required" }),
       password: z.string({ required_error: "password is required" }),
     })
     .strict(),
