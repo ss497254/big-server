3899ddeb0b8add87892b8e610eb65d4a6eb2ed06
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-02 02:49:24 +0530

    update: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,159 +17,145 @@ export interface IEnvVariable<T = EnvVariablesType> {
   type: (typeof acceptedEnvTypes)[number];
   setBy: "default" | "env" | "config-file" | "custom";
   required?: boolean;
-  deleteFromProcess?: boolean;
+  exclusive?: boolean;
 }
 
 const env = {
+  // Server config variables
   HOST: {
     value: "localhost",
     type: "string",
-    setBy: "default",
   } as IEnvVariable<string>,
 
   PORT: {
     value: "8080",
     type: "number",
-    setBy: "default",
   } as IEnvVariable<string>,
 
   PUBLIC_URL: {
     value: undefined,
     type: "string",
-    setBy: "default",
-  } as IEnvVariable<string>,
-
-  JWT_SECRET: {
-    value: "",
-    type: "string",
-    setBy: "default",
-    required: true,
-  } as IEnvVariable<string>,
-
-  USER_REGISTER_SECRET: {
-    value: "",
-    type: "string",
-    setBy: "default",
   } as IEnvVariable<string>,
 
   CONFIG_PATH: {
     value: "config.json",
     type: "string",
-    setBy: "default",
   } as IEnvVariable<string>,
-
-  CORS_ENABLED: {
-    value: false,
-    type: "boolean",
-    setBy: "default",
-  } as IEnvVariable<boolean>,
-
-  CORS_ORIGIN: {
-    value: undefined,
-    type: "string",
-    setBy: "default",
-  } as IEnvVariable<string>,
-
-  CORS_METHODS: {
-    value: "GET,POST,PATCH,DELETE",
-    type: "string",
-    setBy: "default",
-  } as IEnvVariable<string>,
-
-  CORS_ALLOWED_HEADERS: {
-    value: undefined,
-    type: "string",
-    setBy: "default",
-  } as IEnvVariable<string>,
-
-  CORS_EXPOSED_HEADERS: {
-    value: undefined,
-    type: "string",
-    setBy: "default",
-  } as IEnvVariable<string>,
-
-  CORS_CREDENTIALS: {
-    value: true,
-    type: "boolean",
-    setBy: "default",
-  } as IEnvVariable<boolean>,
-
-  CORS_MAX_AGE: {
-    value: 1000 * 60 * 24,
-    type: "number",
-    setBy: "default",
-  } as IEnvVariable<number>,
-
-  SERVER_SHUTDOWN_TIMEOUT: {
-    value: 10 * 1000,
-    type: "number",
-    setBy: "default",
-  } as IEnvVariable<number>,
 
   RATE_LIMITER_GLOBAL_ENABLED: {
     value: false,
     type: "boolean",
-    setBy: "default",
   } as IEnvVariable<boolean>,
 
-  WEBSOCKETS_ENABLED: {
-    value: true,
-    type: "boolean",
-    setBy: "default",
-  } as IEnvVariable<boolean>,
+  SERVER_SHUTDOWN_TIMEOUT: {
+    value: 10 * 1000,
+    type: "number",
+  } as IEnvVariable<number>,
 
   IP_TRUST_PROXY: {
     value: 0,
     type: "number",
-    setBy: "default",
   } as IEnvVariable<number>,
 
   MAX_REQ_PAYLOAD_SIZE: {
     value: 1000 * 100,
     type: "number",
-    setBy: "default",
   } as IEnvVariable<number>,
+
+  // Secrets
+  JWT_SECRET: {
+    value: "",
+    type: "string",
+    required: true,
+  } as IEnvVariable<string>,
+
+  JWT_TOKEN_EXPIRY: {
+    value: 24 * 36 * 36 * 1000,
+    type: "number",
+  } as IEnvVariable<number>,
+
+  USER_REGISTER_SECRET: {
+    value: undefined,
+    type: "string",
+  } as IEnvVariable<string>,
+
+  // cors
+  CORS_ENABLED: {
+    value: false,
+    type: "boolean",
+  } as IEnvVariable<boolean>,
+
+  CORS_ORIGIN: {
+    value: undefined,
+    type: "string",
+  } as IEnvVariable<string>,
+
+  CORS_METHODS: {
+    value: "GET,POST,PATCH,DELETE",
+    type: "string",
+  } as IEnvVariable<string>,
+
+  CORS_ALLOWED_HEADERS: {
+    value: undefined,
+    type: "string",
+  } as IEnvVariable<string>,
+
+  CORS_EXPOSED_HEADERS: {
+    value: undefined,
+    type: "string",
+  } as IEnvVariable<string>,
+
+  CORS_CREDENTIALS: {
+    value: true,
+    type: "boolean",
+  } as IEnvVariable<boolean>,
+
+  CORS_MAX_AGE: {
+    value: 1000 * 60 * 24,
+    type: "number",
+  } as IEnvVariable<number>,
+
+  // web socket
+  WEBSOCKETS_ENABLED: {
+    value: true,
+    type: "boolean",
+  } as IEnvVariable<boolean>,
 
   WEBSOCKETS_HEARTBEAT_PERIOD: {
     value: 5 * 60 * 1000,
     type: "number",
-    setBy: "default",
   } as IEnvVariable<number>,
 
   WEBSOCKETS_ENDPOINT: {
     value: "/ws",
     type: "string",
-    setBy: "default",
   } as IEnvVariable<string>,
 
   WEBSOCKETS_CONN_LIMIT: {
     value: 20,
     type: "number",
-    setBy: "default",
   } as IEnvVariable<number>,
 
+  // firebase
   FIREBASE_CLIENT_EMAIL: {
     value: undefined,
     type: "string",
-    setBy: "default",
     required: true,
-    deleteFromProcess: true,
+    exclusive: true,
   } as IEnvVariable<string>,
 
   FIREBASE_PROJECT_ID: {
     value: undefined,
     type: "string",
-    setBy: "default",
     required: true,
-    deleteFromProcess: true,
+    exclusive: true,
   } as IEnvVariable<string>,
 
   FIREBASE_PRIVATE_KEY: {
     value: undefined,
     type: "string",
-    setBy: "default",
     required: true,
-    deleteFromProcess: true,
+    exclusive: true,
   } as IEnvVariable<string>,
 } as const;
 
@@ -186,8 +172,8 @@ export function getEnv() {
   return env;
 }
 
-export function getEnvConfig(key: EnvKeysType): any {
-  return env[key].value;
+export function getEnvConfig<T = any>(key: EnvKeysType) {
+  return env[key].value as T;
 }
 
 export function setupConfig() {
@@ -196,16 +182,38 @@ export function setupConfig() {
 
   for (const key of keys) {
     if (process.env[key]) {
+      // extract from process
       parseEnv(key, process.env[key], "env");
 
-      // delete important variables from process.env
-      // for security reasons
-      if (env[key].deleteFromProcess) delete process.env[key];
-    } else if (config[key]) parseEnv(key, config[key], "config-file");
-    else if (env[key].required) {
+      // this variable should only be accessed from getEnvConfig
+      if (env[key].exclusive) delete process.env[key];
+    } else if (config[key]) {
+      // load from config file
+      parseEnv(key, config[key], "config-file");
+    } else if (env[key].required) {
       throw new Error(`env variable ${key} is required`);
+    } else {
+      // default value
+      env[key].setBy = "default";
     }
   }
+}
+
+function parseEnv(
+  key: EnvKeysType,
+  value: any,
+  setBy: IEnvVariable<Partial<EnvVariablesType>>["setBy"]
+) {
+  const { type } = env[key];
+
+  if (type === "string") env[key].value = value;
+  else if (type === "boolean") env[key].value = toBoolean(value);
+  else if (type === "number") env[key].value = parseFloat(value);
+  else if (type === "regex") env[key].value = new RegExp(value) as any;
+  else if (type === "json") env[key].value = JSON.parse(value);
+  else throw new Error(`Unexpected Env variable type for ${key}`);
+
+  env[key].setBy = setBy;
 }
 
 function loadConfiguration() {
@@ -237,21 +245,4 @@ function loadConfiguration() {
   throw new Error(
     `Invalid configuration file format. Requires one of "js", "json", received: "${fileExt}"`
   );
-}
-
-function parseEnv(
-  key: EnvKeysType,
-  value: any,
-  setBy: IEnvVariable<Partial<EnvVariablesType>>["setBy"]
-) {
-  const { type } = env[key];
-
-  if (type === "string") env[key].value = value;
-  else if (type === "boolean") env[key].value = toBoolean(value);
-  else if (type === "number") env[key].value = parseFloat(value);
-  else if (type === "regex") env[key].value = new RegExp(value) as any;
-  else if (type === "json") env[key].value = JSON.parse(value);
-  else throw new Error(`Unexpected Env variable type for ${key}`);
-
-  env[key].setBy = setBy;
 }
