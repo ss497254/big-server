3120df48bb4f3cc1fe33bdc3995a095177cc05ad
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-02 01:53:21 +0530

    update: user validations

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,2 +1,1 @@
-export * as adminAuthValidations from "./admin-auth.validation";
 export * as userAuthValidations from "./user-auth.validation";
--- src/validations/user-auth.validation.ts
+++ src/validations/user-auth.validation.ts
@@ -8,3 +8,16 @@ export const userLogin = z.object({
     })
     .strict(),
 });
+
+export const userRegister = z.object({
+  body: z
+    .object({
+      username: z.string({ required_error: "username is required" }),
+      password: z.string({ required_error: "password is required" }),
+      role: z.string({ required_error: "role is required" }),
+      admin: z.boolean().default(false),
+      permission: z.array(z.string()).default([]),
+      secret: z.string({ required_error: "secret is required" }),
+    })
+    .strict(),
+});
