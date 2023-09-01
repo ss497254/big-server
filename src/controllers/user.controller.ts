21996fa04bdc14561e36f976989bb0d6b112107b
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-02 01:27:59 +0530

    fix: async error in user.controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,27 +4,30 @@ import { z } from "zod";
 import { userService } from "src/services";
 import { createAccessToken } from "src/utils/create-access-token";
 import { userAuthValidations } from "src/validations";
+import asyncHandler from "src/utils/async-handler";
 
-export const userLogin = async (
-  data: z.infer<typeof userAuthValidations.userLogin>,
-  res: Response
-) => {
-  const { username, password } = data.body;
+export const userLogin = asyncHandler(
+  async (
+    data: z.infer<typeof userAuthValidations.userLogin>,
+    res: Response
+  ) => {
+    const { username, password } = data.body;
 
-  try {
-    const user = await userService.verifyUserByUsernameAndPassword(
-      username,
-      password
-    );
+    try {
+      const user = await userService.verifyUserByUsernameAndPassword(
+        username,
+        password
+      );
 
-    const token = createAccessToken(user);
+      const token = createAccessToken(user);
 
-    res.send({
-      success: true,
-      message: "Login successful",
-      data: { token },
-    });
-  } catch (e) {
-    throw new InvalidCredentialsError();
+      res.send({
+        success: true,
+        message: "Login successful",
+        data: { token },
+      });
+    } catch (e) {
+      throw new InvalidCredentialsError();
+    }
   }
-};
+);
