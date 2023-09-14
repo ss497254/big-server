ec0760b1c6760a375d9ca54c005eb48ac384c89d
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-14 20:41:14 +0530

    update: error-handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,5 @@
 import { isBigServerError } from "src/errors";
 import type { ErrorRequestHandler } from "express";
-import { ErrorCode, MethodNotAllowedError } from "src/errors";
 import logger from "src/lib/logger";
 import { __prod__ } from "src/config";
 
@@ -9,61 +8,30 @@ const errorHandler: ErrorRequestHandler = (err, req, res, _next) => {
     success: false,
     status: 500,
     message: "INTERNAL_SERVER_ERROR",
-  };
-
-  let errors = [];
-
-  if (!__prod__) {
-    err.extensions = {
-      ...(err.extensions || {}),
+    error: {
+      name: err.name,
+      message: err.message || err?.toString(),
       stack: err.stack,
-    };
-  }
+      cause: err.cause,
+    },
+  } as any;
 
   if (isBigServerError(err)) {
     logger.debug(err.code, err.message);
 
     payload.status = err.status;
-    payload.message = err.message;
-    errors.push(err);
-
-    if (isBigServerError(err, ErrorCode.MethodNotAllowed)) {
-      res.header(
-        "Allow",
-        (
-          err as InstanceType<typeof MethodNotAllowedError>
-        ).extensions.allowed.join(", ")
-      );
-    }
+    payload.message = err.code;
   } else {
-    logger.error(err);
+    logger.error(err.message || err?.toString(), err.stack);
+  }
 
-    errors = [err];
-    if (req.accountability?.admin === true) {
-      errors = [
-        {
-          message: err.message,
-          extensions: {
-            code: "INTERNAL_SERVER_ERROR",
-            ...err.extensions,
-          },
-        },
-      ];
-    } else {
-      errors = [
-        {
-          message: "An unexpected error occurred.",
-          extensions: {
-            code: "INTERNAL_SERVER_ERROR",
-          },
-        },
-      ];
-    }
+  if (req.accountability?.admin !== true && __prod__) {
+    payload.error = undefined;
   }
 
   res.status(payload.status);
 
-  return res.json({ ...payload, errors });
+  return res.json(payload);
 };
 
 export default errorHandler;
