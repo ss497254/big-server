0922dd86b73fe16516d8a458d06ec96fde5c716d
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-14 20:45:46 +0530

    update: admin.route

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,11 +1,21 @@
 import express from "express";
-import * as healthContoller from "src/controllers/health.controller";
+import * as chatsController from "src/controllers/chats.controller";
+import * as adminContoller from "src/controllers/admin.controller";
 import { authenticate } from "src/middleware/authenticate";
+import validate from "src/middleware/validate";
+import { chatsValidations } from "src/validations";
 
 const router = express.Router();
 
 router.use(authenticate({ admin: true }));
 
-router.get("/health-status", healthContoller.getHealthStatus);
+router.get("/health-status", adminContoller.getHealthStatus);
+
+router.get("/active-clients", adminContoller.getActiveClients);
+
+router.post("/create-channel", [
+  validate(chatsValidations.createChannel),
+  chatsController.createChannel,
+]);
 
 export default router;
