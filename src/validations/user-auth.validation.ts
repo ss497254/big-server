680e86f4bc885547c1dd445a27b5677b5e4d8bd9
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-17 16:49:30 +0530

    update: userRegister validation

--- src/validations/user-auth.validation.ts
+++ src/validations/user-auth.validation.ts
@@ -14,9 +14,8 @@ export const userRegister = z.object({
     .object({
       username: z.string({ required_error: "username is required" }),
       password: z.string({ required_error: "password is required" }),
-      role: z.string({ required_error: "role is required" }),
       admin: z.boolean().default(false),
-      permission: z.array(z.string()).default([]),
+      permissions: z.array(z.string()).default([]),
       secret: z.string({ required_error: "secret is required" }),
     })
     .strict(),
