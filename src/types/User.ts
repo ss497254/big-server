83ec70e334686cc116a6d9306e75afd2363296c0
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-28 05:24:35 +0530

    feat: add User interface

---
+++ src/types/User.ts
@@ -0,0 +1,5 @@
+export interface User {
+  username: string;
+  admin?: boolean;
+  permissions: string[];
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
-export * from "./accountability";
+export * from "./Accountability";
 export * from "./jwt-token-payload";
 export * from "./IChannel";
 export * from "./IMessage";
+export * from "./User";
