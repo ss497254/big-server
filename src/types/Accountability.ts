51a07ccb9acada64f9653a81549daf0e1341d1a8
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-28 05:24:03 +0530

    rename: Accountability.ts

--- src/types/Accountability.ts
+++ src/types/Accountability.ts
@@ -1,8 +1,10 @@
-export type Accountability = {
+import { User } from "./User";
+
+export interface Accountability extends User {
   username: string;
   admin?: boolean;
   permissions: string[];
   ip?: string;
   userAgent?: string;
   origin?: string;
-};
+}
