52a3b4e7aed2378d02ab0055df8522d4150039c0
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-05 02:17:22 +0530

    update: simplify things in ws heartbeat handler

--- src/websocket/handlers/heartbeat.ts
+++ src/websocket/handlers/heartbeat.ts
@@ -1,11 +1,9 @@
+import { getEnvConfig } from "src/config";
 import {
   WebSocketController,
   getWebSocketController,
 } from "../controllers/index";
-import { WebSocketMessage } from "../messages";
-import type { WebSocketClient } from "../types";
-import { fmtMessage, getMessageType } from "../utils/message";
-import { getEnvConfig } from "src/config";
+import { fmtMessage } from "../utils/message";
 
 const HEARTBEAT_FREQUENCY = getEnvConfig(
   "WEBSOCKETS_HEARTBEAT_PERIOD"
@@ -18,71 +16,31 @@ export class HeartbeatHandler {
   constructor(controller?: WebSocketController) {
     this.controller = controller ?? getWebSocketController();
 
-    // emitter.onAction("websocket.message", ({ client, message }) => {
-    //   try {
-    //     this.onMessage(client, WebSocketMessage.parse(message));
-    //   } catch {
-    //     /* ignore errors */
-    //   }
-    // });
-
-    // if (toBoolean(env["WEBSOCKETS_HEARTBEAT_ENABLED"]) === true) {
-    //   emitter.onAction("websocket.connect", () => this.checkClients());
-    //   emitter.onAction("websocket.error", () => this.checkClients());
-    //   emitter.onAction("websocket.close", () => this.checkClients());
-    // }
+    this.controller.server.on("connection", this.checkClients);
   }
 
-  private checkClients() {
-    const hasClients = this.controller.clients.size > 0;
+  checkClients() {
+    const hasClients = this.controller.server.clients.size > 0;
 
-    if (hasClients && !this.pulse) {
+    if (this.pulse) {
+      clearInterval(this.pulse);
+      this.pulse = undefined;
+    }
+
+    if (hasClients) {
       this.pulse = setInterval(() => {
         this.pingClients();
       }, HEARTBEAT_FREQUENCY);
     }
-
-    if (!hasClients && this.pulse) {
-      clearInterval(this.pulse);
-      this.pulse = undefined;
-    }
-  }
-
-  onMessage(client: WebSocketClient, message: WebSocketMessage) {
-    if (getMessageType(message) !== "ping") return;
-    // send pong message back as acknowledgement
-    const data = "uid" in message ? { uid: message.uid } : {};
-    client.send(fmtMessage("pong", data));
   }
 
   pingClients() {
-    const pendingClients = new Map<string, WebSocketClient>(
-      this.controller.clients
-    );
-    const activeClients = new Map<string, WebSocketClient>();
+    const clients = this.controller.server.clients;
 
-    const timeout = setTimeout(() => {
-      // close connections that haven't responded
-      for (const client of pendingClients.values()) {
-        client.close();
-      }
-    }, HEARTBEAT_FREQUENCY);
-
-    const messageWatcher = (user: string, client: WebSocketClient) => {
-      // any message means this connection is still open
-      if (!activeClients.has(user)) {
-        pendingClients.delete(user);
-        activeClients.set(user, client);
-      }
-
-      if (pendingClients.size === 0) {
-        clearTimeout(timeout);
-      }
-    };
-
-    for (const client of pendingClients.values()) {
+    for (const client of clients.values()) {
       client.send(fmtMessage("ping"));
-      client.on("message", messageWatcher);
     }
+
+    this.checkClients();
   }
 }
