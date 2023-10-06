70e18f545b6cba0713e8761e76d4488f759f2993
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-06 16:39:39 +0530

    update: add route to get user details

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,8 +2,11 @@ import * as userController from "src/controllers/user.controller";
 import express from "express";
 import validate from "src/middleware/validate";
 import { userAuthValidations } from "src/validations";
+import { authenticate } from "src/middleware/authenticate";
 
 const router = express.Router();
+
+router.get("/me", [authenticate(), userController.getMe]);
 
 router.post("/login", [
   validate(userAuthValidations.userLogin),
