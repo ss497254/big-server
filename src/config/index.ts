e96c2c2e397b87ffc503a39f874ab11a18534b12
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-06 18:28:36 +0530

    update: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,184 +1,33 @@
 import fs from "fs";
 import path from "path";
 import { toBoolean } from "src/utils/to-boolean";
-
-const acceptedEnvTypes = [
-  "string",
-  "number",
-  "boolean",
-  "regex",
-  "json",
-] as const;
+import { EnvVariablesType, IEnvVariable } from "./types";
+import { websocketEnv } from "./websocket";
+import { corsEnv } from "./cors";
+import { firebaseEnv } from "./firebase";
+import { pingEnv } from "./ping";
+import { serverEnv } from "./server";
+import { rateLimiterEnv } from "./rate-limiter";
+import { secretEnv } from "./secrets";
 
 export const __prod__ = process.env.NODE_ENV === "production";
 
-export interface IEnvVariable<T = EnvVariablesType> {
-  value: T | undefined;
-  type: (typeof acceptedEnvTypes)[number];
-  setBy: "default" | "env" | "config-file" | "custom";
-  required?: boolean;
-  exclusive?: boolean;
-}
-
 const env = {
-  // Server config variables
-  HOST: {
-    value: "127.0.0.1",
-    type: "string",
-  } as IEnvVariable<string>,
-
-  PORT: {
-    value: 8080,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  PUBLIC_URL: {
-    value: undefined,
-    type: "string",
-  } as IEnvVariable<string>,
-
-  PING_URL: {
-    value: undefined,
-    type: "string",
-  } as IEnvVariable<string>,
-
   CONFIG_PATH: {
     value: "config.json",
     type: "string",
   } as IEnvVariable<string>,
 
-  RATE_LIMITER_GLOBAL_ENABLED: {
-    value: false,
-    type: "boolean",
-  } as IEnvVariable<boolean>,
-
-  SERVER_SHUTDOWN_TIMEOUT: {
-    value: 10 * 1000,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  IP_TRUST_PROXY: {
-    value: 0,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  MAX_REQ_PAYLOAD_SIZE: {
-    value: 1000 * 1000,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  // Secrets
-  JWT_SECRET: {
-    value: "",
-    type: "string",
-    required: true,
-  } as IEnvVariable<string>,
-
-  JWT_TOKEN_EXPIRY: {
-    value: 24 * 36 * 36 * 1000,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  USER_REGISTER_SECRET: {
-    value: undefined,
-    type: "string",
-  } as IEnvVariable<string>,
-
-  // cors
-  CORS_ENABLED: {
-    value: true,
-    type: "boolean",
-  } as IEnvVariable<boolean>,
-
-  CORS_ORIGIN: {
-    value: "*",
-    type: "string",
-  } as IEnvVariable<string>,
-
-  CORS_METHODS: {
-    value: "GET,POST,PATCH,DELETE",
-    type: "string",
-  } as IEnvVariable<string>,
-
-  CORS_ALLOWED_HEADERS: {
-    value: undefined,
-    type: "string",
-  } as IEnvVariable<string>,
-
-  CORS_EXPOSED_HEADERS: {
-    value: undefined,
-    type: "string",
-  } as IEnvVariable<string>,
-
-  CORS_CREDENTIALS: {
-    value: false,
-    type: "boolean",
-  } as IEnvVariable<boolean>,
-
-  CORS_MAX_AGE: {
-    value: 1000 * 60 * 24,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  // web socket
-  WEBSOCKETS_ENABLED: {
-    value: true,
-    type: "boolean",
-  } as IEnvVariable<boolean>,
-
-  WEBSOCKETS_HEARTBEAT_PERIOD: {
-    value: 5 * 60 * 1000,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  WEBSOCKETS_ENDPOINT: {
-    value: "/ws",
-    type: "string",
-  } as IEnvVariable<string>,
-
-  WEBSOCKETS_CONN_LIMIT: {
-    value: 20,
-    type: "number",
-  } as IEnvVariable<number>,
-
-  // firebase
-  FIREBASE_CLIENT_EMAIL: {
-    value: undefined,
-    type: "string",
-    required: true,
-    exclusive: true,
-  } as IEnvVariable<string>,
-
-  FIREBASE_PROJECT_ID: {
-    value: undefined,
-    type: "string",
-    required: true,
-    exclusive: true,
-  } as IEnvVariable<string>,
-
-  FIREBASE_BUCKET_NAME: {
-    value: undefined,
-    type: "string",
-    required: true,
-    exclusive: true,
-  } as IEnvVariable<string>,
-
-  FIREBASE_PRIVATE_KEY: {
-    value: undefined,
-    type: "string",
-    required: true,
-    exclusive: true,
-  } as IEnvVariable<string>,
+  ...serverEnv,
+  ...rateLimiterEnv,
+  ...secretEnv,
+  ...pingEnv,
+  ...corsEnv,
+  ...websocketEnv,
+  ...firebaseEnv,
 } as const;
 
 export type EnvKeysType = keyof typeof env;
-export type EnvVariablesType =
-  | number
-  | string
-  | boolean
-  | RegExp
-  | Record<string, any>
-  | Record<string, any>[];
 
 export function getEnv() {
   return env;
